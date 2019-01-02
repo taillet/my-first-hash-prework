f535d631edc4314e8aafcbe63de738fa4566077b
Heloise Taillet <ht2394@columbia.edu>
2019-01-02 16:50:05 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -3,22 +3,22 @@ def my_hash
   hash = {"key1" => "value1", "key2" => "value2"}
 
 
-  
+
 end
 
 
 def shipping_manifest
   # set a variable called `the_manifest`, equal to a hash
-  # fill that hash with key/value pairs that describe the following information: 
-  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
+  # fill that hash with key/value pairs that describe the following information:
+  # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
+the_manifest = {"wb1" => "v1", "wb2" => "v2", "wb3" => "v3", "wb4" => "v4", "wb5" =>"v5", "op1" => "v6", "op2" => "v7", "op3" => "v8" }
 
 end
 
 def retrieval
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
 
@@ -28,8 +28,8 @@ end
 
 def adding
   shipping_manifest = {
-                  "whale bone corsets" => 5, 
-                  "porcelain vases" => 2, 
+                  "whale bone corsets" => 5,
+                  "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
